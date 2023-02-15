c32f394b066ac9a37d12f8776daa1efd6a9ca97f
Jethro <jethrotiago@hotmail.com>
2023-02-15 16:43:16 -0300

    Acrescentado mensagem throw Error

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/');
+var consultaCEP = fetch('https://viacep.com.br/ws/01001250/json/')
+.then(resposta => resposta.json())
+.then(r => {
+    if (r.erro) {
+        throw Error('Esse CEP não existe!')
+    } else
+        console.log(r)
+})
+.catch(erro => console.log(erro))
+.finally(mensagem => console.log('Processamento concluido!'));
 
 console.log(consultaCEP);
