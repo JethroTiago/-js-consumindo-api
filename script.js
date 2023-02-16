7b9cb156bc2178345cb547e1f51b51088742a405
Jethro <jethrotiago@hotmail.com>
2023-02-16 13:20:43 -0300

    Criando função assincrona

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
+async function buscaEndereco() {
+    var consultaCEP = await fetch('https://viacep.com.br/ws/01001000/json/')
+    var consultaCEPconvertida = await consultaCEP.json();
+    console.log(consultaCEPconvertida);
+}
 
-console.log(consultaCEP);
+buscaEndereco();
