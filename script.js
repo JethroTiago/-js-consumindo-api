456c3f97f1921c0a2f3c5bfbe7de273ff48df899
Jethro <jethrotiago@hotmail.com>
2023-02-16 14:18:49 -0300

    Adicionado template string no await fetch

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-async function buscaEndereco() {
+async function buscaEndereco(cep) {
     try {
-        var consultaCEP = await fetch('https://viacep.com.br/ws/01001000/json/')
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
         var consultaCEPconvertida = await consultaCEP.json();
         if (consultaCEPconvertida.erro) {
             throw Error('CEP não existente!');
         }
         console.log(consultaCEPconvertida);
+        return consultaCEPconvertida;
     } catch (erro) {
         console.log(erro);
     }
 }
 
+let ceps = ['01001000', '01001001'];
+let consjutoCEPS = ceps.map(valores => buscaEndereco(valores));
+Promise.all(consjutoCEPS).then(respostas => console.log(respostas));
 buscaEndereco();
