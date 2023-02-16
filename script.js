b7e0822fc09684a19ef9d315b4530acadc0c2ac3
Jethro <jethrotiago@hotmail.com>
2023-02-16 13:31:06 -0300

    Criando condição para erros

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 async function buscaEndereco() {
-    var consultaCEP = await fetch('https://viacep.com.br/ws/01001000/json/')
-    var consultaCEPconvertida = await consultaCEP.json();
-    console.log(consultaCEPconvertida);
+    try {
+        var consultaCEP = await fetch('https://viacep.com.br/ws/01001000/json/')
+        var consultaCEPconvertida = await consultaCEP.json();
+        if (consultaCEPconvertida.erro) {
+            throw Error('CEP não existente!');
+        }
+        console.log(consultaCEPconvertida);
+    } catch (erro) {
+        console.log(erro);
+    }
 }
 
 buscaEndereco();
